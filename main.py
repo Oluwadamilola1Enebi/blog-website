bf1a0f16a9ad94ee15a4dd9eba8d615b6934dc26
Oluwadamilola Enebi <oluwadamilolaenebi@gmail.com>
2022-10-31 18:54:29 +0100

    PostgreSQL Update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
@@ -15,7 +16,7 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 #FOR GRAVATAR COMMENT ICON PICTURES
@@ -29,7 +30,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
